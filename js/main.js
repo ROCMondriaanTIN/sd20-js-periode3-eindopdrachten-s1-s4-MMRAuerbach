cadfd44f585fd871657a86a8e0ba475e00f8519d
MMR Auerbach <m.auerbach@rocmondriaan.nl>
2021-03-12 10:05:49 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// Dit bestand is nu leeg, het is aan jullie om het te vullen :-D Succes!
+//Declare and initialize two variables that do not change
+const myName = "Michiel";
+const myNumber = 200938475;
+
+//Function to show the data in an alert box
+function showMyInfo() {
+    alert(`My name is ${myName} and my number is ${myNumber}`);
+}
+
+//Call the function with the name showMyInfo
+showMyInfo();
